305697daad3514acfa60a74680241cd5da38d6f9
Deepak V S <deepaksanjeev1998@gmail.com>
2024-07-11 22:27:35 +0530

    deleted some u parts

--- client/src/components/pages/students/studentnavdemo.tsx
+++ client/src/components/pages/students/studentnavdemo.tsx
@@ -20,21 +20,7 @@ const Links = [
 ]
 
 
-const Nav = () => {
-    const location = useLocation();
-    return (
-        <nav className="flex gap-8">
-            {Links.map((link, index) => { 
-                return(
-                    <>
-                        <button key={index} className={`${link?.path === location?.pathname && "text-red border-b-2 border-red-950" } capitalize hover:text-blue-400 transition-all`} >{link.name }</button>
-                    </>
-                )
-            })}
-        
-        </nav>
-    )
-}
+// git 
 
 
 
